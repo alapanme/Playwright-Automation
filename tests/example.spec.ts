774d39f3f79557f174955a9ae2da71046d912dea
alapanme <25705483+alapanme@users.noreply.github.com>
2022-07-29 10:03:34 +0300

    playwright upgrade to 1.24.1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await page.locator('text=Get started').click();
-  await expect(page).toHaveTitle(/Getting started/);
+  await expect(page).toHaveTitle(/Installation | Playwright/);
 });
